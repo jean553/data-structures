e4d62be8fbfdab02e21830b8731e9029a44dec86
jean <jean@bst>
2017-03-16 22:38:22 +0800

    attempt to integrate the push function

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -2,32 +2,55 @@ extern crate core;
 
 use core::default::Default;
 
-struct Node<'a> {
-    data: u64,
-    next: Option<&'a Node<'a>>
+struct Node {
+    data: u32,
+    next: Option<Box<Node>>
 }
 
-#[derive(Default)]
-struct LinkedList<'a> {
-    head: Option<Node<'a>>
-}
+impl Node {
 
-impl<'a> LinkedList<'a> {
-    
-    fn insert_at_begin(&mut self, data: u64) {
-
-        let node = Node{
+    pub fn new(data: u32) -> Self {
+        Node {
             data: data,
             next: None
-        };
-        
-        self.head = Some(node);
+        }
+    }
+}
+
+struct LinkedList {
+    head: Option<Box<Node>>,
+}
+
+impl LinkedList {
+
+    pub fn new() -> Self {
+        LinkedList {
+            head: None,
+        }
+    }
+
+    pub fn push(&mut self, data: u32) {
+
+        let ref mut current = self.head;
+
+        loop {
+
+            current = match *current {
+                None => {
+                    &mut Some(Box::new(Node::new(data)))
+                }
+                Some(ref cur) => {
+                    &mut cur.next
+                }
+            }
+        }
     }
 }
 
 fn main() {
 
-    let mut list = LinkedList{..Default::default()};
+    let mut list = LinkedList::new();
 
-    list.insert_at_begin(10);
+    list.push(10);
+    list.push(20);
 }
